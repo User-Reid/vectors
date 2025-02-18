8e71df93162a07b7a4b9a4074f2668c86d6ecac3
Reid Herrera <reidherrerabusiness@gmail.com>
2025-02-18 16:19:44 -0600

    first commit

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let pizza_diameters: Vec<u8> = vec![8, 10, 12, 14];
+
+    println!("{pizza_diameters:?}");
+
+    let pastas = vec!["Rigatoni", "Angel Hair", "Fettucine"];
+    println!("{pastas:?}");
+}
