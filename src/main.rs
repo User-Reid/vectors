7cbc08926f8ac45e999eeacf959c4718aa35263f
Reid Herrera <reidherrerabusiness@gmail.com>
2025-02-18 20:34:55 -0600

    reading vector elements

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
 fn main() {
     let mut pizza_diameters: Vec<u8> = vec![8, 10, 12, 14];
-    pizza_diameters.push(12);
-    println!("{pizza_diameters:?}");
 
-    pizza_diameters.insert(2, 31);
-    println!("{pizza_diameters:?}");
+    let pepperoni: String = String::from("Pepperoni");
+    let mushroom: String = String::from("Mushroom");
+    let sausage: String = String::from("Sausage");
+    let pizza_toppings: Vec<String> = vec![pepperoni, mushroom, sausage];
 
-    let last_pizza_diamater = pizza_diameters.pop();
-    println!("{:?}", last_pizza_diamater.unwrap());
-
-    pizza_diameters.remove(3);
-    println!("{:?}", pizza_diameters)
+    let value = &pizza_toppings[1..];
+    println!("{value:?}")
 }
