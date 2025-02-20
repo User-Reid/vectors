8735454ef1cfb404d94943dd1953b16f53458045
Reid Herrera <reidherrerabusiness@gmail.com>
2025-02-20 10:16:14 -0600

    understanding vector capacity :)

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
 fn main() {
-    let pepperoni: String = String::from("Pepperoni");
-    let mushroom: String = String::from("Mushroom");
-    let sausage: String = String::from("Sausage");
-    let mut pizza_toppings: Vec<String> = vec![pepperoni, mushroom, sausage];
+    let mut seasons: Vec<&str> = Vec::with_capacity(4);
+    println!(
+        "Length: {}, Capacity: {}",
+        seasons.len(),
+        seasons.capacity()
+    );
 
-    pizza_toppings[1] = String::from("Olives");
-    println!("{pizza_toppings:?}");
+    seasons.push("Summer");
+    seasons.push("Spring");
+    seasons.push("Fall");
+    seasons.push("Winter");
 
-    let target_topping: &mut String = &mut pizza_toppings[2];
-    target_topping.push_str(" and Meatballs");
-    let x: &mut String = &mut pizza_toppings[1];
-    println!("{}", x);
-    let y: &String = &pizza_toppings[1];
-    println!("{pizza_toppings:?}");
+    println!("{seasons:?}");
+    println!(
+        "Length: {}, Capacity: {}",
+        seasons.len(),
+        seasons.capacity()
+    );
+
+    seasons.push("Banana");
+    println!("{seasons:?}");
+    println!(
+        "Length: {}, Capacity: {}",
+        seasons.len(),
+        seasons.capacity()
+    );
 }
