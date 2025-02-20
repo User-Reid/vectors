102370fa1517eec77804a84cf67d5f4e39333087
Reid Herrera <reidherrerabusiness@gmail.com>
2025-02-19 23:06:39 -0600

    understanding ownership with vectors

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 fn main() {
-    let mut pizza_diameters: Vec<u8> = vec![8, 10, 12, 14];
-
     let pepperoni: String = String::from("Pepperoni");
     let mushroom: String = String::from("Mushroom");
     let sausage: String = String::from("Sausage");
-    let pizza_toppings: Vec<String> = vec![pepperoni, mushroom, sausage];
 
-    let option = pizza_toppings.get(2);
-    println!("{}", option.unwrap());
+    let pizza_toppings: Vec<String> = vec![pepperoni, mushroom, sausage];
+    let mut delicious_toppings: Vec<String> = pizza_toppings;
+
+    let topping_reference: &String = &delicious_toppings[1];
+    println!("The topping is {topping_reference}");
+
+    delicious_toppings.push(String::from("Olives"));
 }
