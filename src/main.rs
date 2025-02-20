15ba10c74f5e76b32942148bc3a74f1a912e1719
Reid Herrera <reidherrerabusiness@gmail.com>
2025-02-19 23:09:11 -0600

    understanding how to write vector elements pt 1

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ fn main() {
     let pepperoni: String = String::from("Pepperoni");
     let mushroom: String = String::from("Mushroom");
     let sausage: String = String::from("Sausage");
+    let mut pizza_toppings: Vec<String> = vec![pepperoni, mushroom, sausage];
 
-    let pizza_toppings: Vec<String> = vec![pepperoni, mushroom, sausage];
-    let mut delicious_toppings: Vec<String> = pizza_toppings;
+    pizza_toppings[1] = String::from("Olives");
+    println!("{pizza_toppings:?}");
 
-    let topping_reference: &String = &delicious_toppings[1];
-    println!("The topping is {topping_reference}");
-
-    delicious_toppings.push(String::from("Olives"));
+    let target_topping: &mut String = &mut pizza_toppings[2];
+    target_topping.push_str(" and Meatballs");
+    println!("{pizza_toppings:?}");
 }
